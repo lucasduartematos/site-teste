500d663697b2903fafb1761dde96d833968600dc
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-03-13 21:52:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é meu site. (Lucas Duarte)"
+
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da página Contato"
