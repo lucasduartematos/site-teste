7bfb6fb9fa2cebab78437eb1deb6889a0708c2a2
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-03-14 11:59:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello_world():
-  return "Olá, mundo! Esse é meu site. (Lucas Duarte)"
 
-@app.route("/contato")
-def contato():
-  return "Aqui vai o conteúdo da página Contato"
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "Olá, mundo! Esse é meu site. (Lucas Duarte)"
